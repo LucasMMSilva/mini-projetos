d36039a9ae67bb8c7c150a53eb3d00a7434b2048
Lucas Manoel <dev.lucasmanoel@gmail.com>
2022-11-16 19:54:38 -0300

    Refatoração da função Convert

--- 01-converter-algarismo-romano/script.js
+++ 01-converter-algarismo-romano/script.js
@@ -2,7 +2,7 @@ var inputAria = document.querySelector('#inputAria');
 var selectInput = document.querySelector('#selectInput').value;
 var romanicValue = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000};
 function convert(){
-    if(selectInput.value == 'ar-d'){
+    if(selectInput == 'ar-d'){
         inputAria.value = convertToDecimal().toString();
     }else{
 
@@ -14,16 +14,12 @@ function convertToDecimal(){
     
     let result = 0;
     for(let i = 0;i <= romanicArray.length-1;i++){
-        if(romanicValue[romanicArray[i+1]]){
             if(romanicValue[romanicArray[i]]<romanicValue[romanicArray[i+1]]){
-                result += romanicValue[romanicArray[i+1]] - romanicValue[romanicArray[i]]     
-                i++;   
+                result -= romanicValue[romanicArray[i]]     
             }else{
                 result += romanicValue[romanicArray[i]];
             }
-        }else{
-            result += romanicValue[romanicArray[i]];
-        }
+
     }
     return result
 }
