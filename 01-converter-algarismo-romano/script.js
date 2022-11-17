67317dc6989d693d0eebc069754b18a86ea170ec
Lucas Manoel <dev.lucasmanoel@gmail.com>
2022-11-17 17:36:53 -0300

    decimal to romanic number update

--- 01-converter-algarismo-romano/script.js
+++ 01-converter-algarismo-romano/script.js
@@ -1,13 +1,34 @@
-var inputAria = document.querySelector('#inputAria');
-var selectInput = document.querySelector('#selectInput').value;
-var romanicValue = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000};
-function convert(){
-    if(selectInput == 'ar-d'){
-        inputAria.value = convertToDecimal().toString();
-    }else{
+var inputAria     =  document.querySelector('#inputAria');
+var selectInput   =  document.querySelector('#selectInput');
+var romanicValue  =  {'':0,'I':1,'II':2,'III':3,'IV':4,'V':5,'VI':6,'VII':7,'VIII':8,'IX':9,
+                    'X':10,'XX':20,'XXX':30,'XL':40,'L':50,'LX':60,'LXX':70,'LXXX':80,'XC':90,
+                    'C':100,'CC':200,'CCC':300,'CD':400,'D':500,'DC':600,'DCC':700,'DCCC':800,'CM':900,
+                    'M':1000,'MM':2000,'MMM':3000};
 
+function convert(){
+    if(selectInput.value == 'ar-d'){
+        inputAria.value = convertToDecimal().toString();
+    }else if(selectInput.value == 'd-ar'){
+        inputAria.value = convertToRoman().toString();
     }
-    
+}
+function convertToRoman(){
+    if(inputAria.value < 4000){
+        let numeralArray = inputAria.value.split(''); 
+        let varAux = 0;
+        var romanAux = [];
+        for(let i = 0;i < numeralArray.length;i++){
+            for(let j = 0; j < (numeralArray.length-1)-varAux;j++){
+                numeralArray[i]=parseInt((numeralArray[i]*10));  
+            }
+            romanAux.push(Object.keys(romanicValue).find(key => romanicValue[key] == numeralArray[i]))
+            varAux++;
+        }
+        return romanAux.join('')
+    }else{
+        window.alert('Numero maximo é 3999, por favor tente um valor menor!')
+        return ''
+    }
 }
 function convertToDecimal(){
     let romanicArray = inputAria.value.toUpperCase().split('');
