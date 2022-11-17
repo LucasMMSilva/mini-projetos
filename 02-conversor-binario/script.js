e1f6a1bf39733a8dc4cdc92ba9f2fffef69f54e7
Lucas Manoel <dev.lucasmanoel@gmail.com>
2022-11-17 17:51:17 -0300

    Upload file conversor binary

---
+++ 02-conversor-binario/script.js
@@ -0,0 +1,16 @@
+var inputAria     =  document.querySelector('#inputAria');
+var selectInput   =  document.querySelector('#selectInput');
+
+function convert(){
+    if(selectInput.value == 'b-d'){
+        inputAria.value = '';
+    }else if(selectInput.value == 'd-b'){
+        inputAria.value = '';
+    }
+}
+function convertToBinary(){
+    
+}
+function convertToDecimal(){
+   
+}
