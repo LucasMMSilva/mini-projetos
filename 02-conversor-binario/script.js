8389609e83ac5d21a789e0f5499cbeec1cc61bba
Lucas Manoel <dev.lucasmanoel@gmail.com>
2022-11-19 19:51:19 -0300

    Update Binary to Decimal

--- 02-conversor-binario/script.js
+++ 02-conversor-binario/script.js
@@ -4,7 +4,7 @@ var selectInput   =  document.querySelector('#selectInput');
 
 function convert(){
     if(selectInput.value == 'b-d'){
-        inputAria.value = '';
+        inputAria.value = convertToDecimal().toString();
     }else if(selectInput.value == 'd-b'){
         inputAria.value = convertToBinary().toString();
     }
@@ -14,21 +14,25 @@ function convertToBinary(){
     let newBinaryArray = [];
     let result = inputAria.value;
     while(result != 1){
-        console.log(`Entranda: ${result}`)
-        binaryArray.push(result%2)
-        result = parseInt(result/2)
-        
-        console.log(`Resultado: ${result}`)
-        console.log(`Resto: ${result%2}`)
-        console.log('--------------------------------------------')
+        binaryArray.push(result%2);
+        result = parseInt(result/2);
     }
     binaryArray.push(result);
     while(binaryArray.length != 0){
         newBinaryArray.push(binaryArray.pop());
     }
-    console.log(`${inputAria.value} = ${newBinaryArray.join('')}`);
     return newBinaryArray.join('');
 }
 function convertToDecimal(){
-   
+    let binaryArray = inputAria.value.split('');
+    let multiplicador = 1;
+    let result = 0;
+    while(binaryArray.length!=0){
+        let aux = binaryArray.pop()
+        if(aux == 1){
+            result+=multiplicador
+        }
+        multiplicador*=2;
+    }
+    return result
 }
