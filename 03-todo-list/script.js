6f2ceb34149bf2227c9eae27883c9fd518b5f88b
Lucas Manoel <dev.lucasmanoel@gmail.com>
2022-11-23 20:17:19 -0300

    div container de task

--- 03-todo-list/script.js
+++ 03-todo-list/script.js
@@ -2,6 +2,6 @@ let nameTask = document.getElementById('nameTask')
 
 nameTask.addEventListener("keyup", ({key}) => {
     if (key === "Enter") {
-        console.log(nameTask.value)
+        localStorage.setItem('taskName',nameTask.value)
     }
 })
