7b312db9d0b6a329424faa56c87d0db42cf3f3ba
Lucas Manoel <dev.lucasmanoel@gmail.com>
2022-11-24 19:09:23 -0300

    update add task

--- 03-todo-list/script.js
+++ 03-todo-list/script.js
@@ -1,7 +1,24 @@
-let nameTask = document.getElementById('nameTask')
+let nameTask = document.getElementById('nameTask');
+var storage = [];
+
+const getStorage = ()=> JSON.parse(localStorage.getItem('todoList')) ?? [''];
+const setStorage = (storage)=> localStorage.setItem('todoList',JSON.stringify(storage));
+
+function createNewItem(index,task,status){
+    const taskObj = document.createElement('label');
+    taskObj.classList.add('task');
+    taskObj.innerHTML = `
+        <input type="checkbox" ${status} data-index=${index}>
+        <label>${task}</label>
+        <button data-index=${index}>&#215;</button>
+    `;
+    document.getElementById('taskList').appendChild(taskObj);
+}
+
 
 nameTask.addEventListener("keyup", ({key}) => {
     if (key === "Enter") {
-        localStorage.setItem('taskName',nameTask.value)
+        createNewItem(1,nameTask.value,"")
+        nameTask.value = "";
     }
 })
