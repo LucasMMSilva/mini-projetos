388c46da4189d471f6166f03fe5f74f32bd33207
Lucas Manoel <dev.lucasmanoel@gmail.com>
2022-12-01 18:47:43 -0300

    Refatoração do html e css

--- 03-todo-list/script.js
+++ 03-todo-list/script.js
@@ -1,26 +1,21 @@
-/*let nameTask = document.getElementById('nameTask');
+let nameTask = document.getElementById('nameTask');
 var storage = [];
 
 const getStorage = ()=> JSON.parse(localStorage.getItem('todoList')) ?? [''];
 const setStorage = (storage)=> localStorage.setItem('todoList',JSON.stringify(storage));
 
-function createNewItem(index,task,status){
-    const taskObj = document.createElement('label');
+function createNewItem(index,task){
+    const taskObj = document.createElement('div');
     if(nameTask.value != ''){
         taskObj.classList.add('task');
-        taskObj.innerHTML = `
-            <input type="checkbox" ${status} data-index=${index}>
-            <label>${task}</label>
-            <button data-index=${index}>&#215;</button>
-        `;
-        document.getElementById('taskList').appendChild(taskObj);
+        taskObj.innerHTML = `<label>${task}</label>`;
+        document.getElementById('list').appendChild(taskObj);
     }
 }
 
-
 nameTask.addEventListener("keyup", ({key}) => {
     if (key === "Enter") {
-        createNewItem(1,nameTask.value,"")
+        createNewItem(1,nameTask.value);
         nameTask.value = "";
     }
-})*/
+})
