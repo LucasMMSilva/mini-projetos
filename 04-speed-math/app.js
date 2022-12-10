e632f351463c2930e2f5e3dac817bb7213edb41c
Lucas Manoel <dev.lucasmanoel@gmail.com>
2022-12-09 22:20:48 -0300

    recebendo elementos

--- 04-speed-math/app.js
+++ 04-speed-math/app.js
@@ -3,11 +3,19 @@ const telaParametros2 = document.getElementById('param-2');
 const telaMain = document.getElementById('main');
 const telaResult = document.getElementById('result');
 
+let inputText = document.getElementById('inpText');
+let adicao = document.getElementById('adicao');
+let subtracao = document.getElementById('subtracao');
+let multiplicacao = document.getElementById('multiplicacao');
+let divisao = document.getElementById('divisao');
+
+
+
 telaParametros2.style.display = "none"
 telaMain.style.display = "none"
 telaResult.style.display = "none"
 
-
+function getRadio(){return document.querySelector('input[name="dig"]:checked').value;}
 function next(){
 
 
@@ -42,7 +50,7 @@ function tryAgain(){
 }
 function createNew(){
 
-    
+
     telaParametros1.style.display = "block"
     telaParametros2.style.display = "none"
     telaMain.style.display = "none"
