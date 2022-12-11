07bb5ce7e21841650490e46e5821829506e9b2ca
Lucas Manoel <dev.lucasmanoel@gmail.com>
2022-12-10 22:04:43 -0300

    variaveis update

--- 04-speed-math/app.js
+++ 04-speed-math/app.js
@@ -1,58 +1,61 @@
-const telaParametros1 = document.getElementById('param-1');
-const telaParametros2 = document.getElementById('param-2');
-const telaMain = document.getElementById('main');
-const telaResult = document.getElementById('result');
+const telaParametros1  =  document.getElementById('param-1');
+const telaParametros2  =  document.getElementById('param-2');
+const telaMain         =  document.getElementById('main');
+const telaResult       =  document.getElementById('result');
+let inputText          =  document.getElementById('inpText');
+let adicao             =  document.getElementById('adicao');
+let subtracao          =  document.getElementById('subtracao');
+let multiplicacao      =  document.getElementById('multiplicacao');
+let divisao            =  document.getElementById('divisao');
 
-let inputText = document.getElementById('inpText');
-let adicao = document.getElementById('adicao');
-let subtracao = document.getElementById('subtracao');
-let multiplicacao = document.getElementById('multiplicacao');
-let divisao = document.getElementById('divisao');
 
+let acertos    = 0;
+let erros      = 0;
 
 
 telaParametros2.style.display = "none"
-telaMain.style.display = "none"
-telaResult.style.display = "none"
+telaMain.style.display        = "none"
+telaResult.style.display      = "none"
 
 function getRadio(){return document.querySelector('input[name="dig"]:checked').value;}
+
 function next(){
 
 
     telaParametros1.style.display = "none"
     telaParametros2.style.display = "block"
-    telaMain.style.display = "none"
-    telaResult.style.display = "none"
+    telaMain.style.display        = "none"
+    telaResult.style.display      = "none"
 }
 function start(){
 
 
     telaParametros1.style.display = "none"
     telaParametros2.style.display = "none"
-    telaMain.style.display = "block"
-    telaResult.style.display = "none"
+    telaMain.style.display        = "block"
+    telaResult.style.display      = "none"
 }
 function calc(){
 
 
     telaParametros1.style.display = "none"
     telaParametros2.style.display = "none"
-    telaMain.style.display = "none"
-    telaResult.style.display = "block"
+    telaMain.style.display        = "none"
+    telaResult.style.display      = "block"
 }
 function tryAgain(){
 
 
     telaParametros1.style.display = "none"
     telaParametros2.style.display = "none"
-    telaMain.style.display = "block"
-    telaResult.style.display = "none"
+    telaMain.style.display        = "block"
+    telaResult.style.display      = "none"
 }
 function createNew(){
 
 
     telaParametros1.style.display = "block"
     telaParametros2.style.display = "none"
-    telaMain.style.display = "none"
-    telaResult.style.display = "none"
+    telaMain.style.display        = "none"
+    telaResult.style.display      = "none"
 }
